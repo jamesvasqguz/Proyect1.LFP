b8ee15b2e3cee6f25625b7963921f0b0e8bcb311
James Vasquez <guzmanrandy.29@gmail.com>
2019-07-29 15:22:34 -0600

    "2.0"

--- Logic.js
+++ Logic.js
@@ -1,23 +1,99 @@
-var james= document.getElementById("bVerifier");
-var historialOP = document.getElementById("historialOP");
+var arrayWord = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var arrayNumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var arraySymbol = ['#', '$', '%'];
 
-function logicV(){
-    for(var i=0;i<james.value.length;i++){
-    console.log(james.value[i]);        
-    switch(james.value[i]){
-        case 'a':
-            alert("El dato ingresado es una letra: " +james.value); 
-        break;
-        case '1':
-            alert("El dato ingresado es un numero: " +james.value); 
-        break;
-        case '#':    
-            alert("El dato ingresado es un simbolo: " +james.value); 
-        break;
-        default:
-            alert("Error ");
-        break;
+var james = document.getElementById("bVerifier");
+var historialOP = document.getElementById("historialOP");
+var a;
+function logicV() {
+    var finalizar = true;
+    var iden = true;
+    var num = true;
+    var numer = true;
+    var sym = true;
+
+    if (finalizar == true && iden == true) {
+        isIdentifier();
+    }
+    if (finalizar == true && num == true) {
+        isNumber();
+    }
+    if (finalizar == true && numer == true) {
+        isNumberError();
+    }
+    if (finalizar == true && sym == true) {
+        isSymbol();
+    }
+}
+function jara() {
+    historialOP.innerHTML = historialOP.innerHTML + james.value + a + '<br>';
+}
+
+function isIdentifier() {
+    for (var i = 0; i < james.value.length; i++) {
+        for (var j = 0; j < arrayWord.length; j++) {
+            for (var m = 0; m < arrayNumber.length; m++) {
+                if (james.value[0] == arrayWord[j] && (james.value[i] == arrayNumber[m] ||
+                    james.value[i] == arrayWord[j])) {
+                    a = ' Identifier';
+                    jara();
+                    finalizar == false;
+                    num == false;
+                    numer == false;
+                    sym == false;
+                    return alert("It's an Identifier " + james.value);
+                }
+            }
         }
-    } 
-    historialOP.innerHTML = historialOP.innerHTML +james.value +'<br>';      
+    }
+}
+function isNumber() {
+    for (var i = 0; i < james.value.length; i++) {
+        for (var j = 0; j < arrayNumber.length; j++) {
+            for (var m = 0; m < arrayWord.length; m++) {
+                if (james.value[0] == arrayNumber[j] && !james.value[i] == arrayWord[m]) {
+                    a = ' Number';
+                    jara();
+                    finalizar == false;
+                    numer == false;
+                    sym == false;
+                    iden == false;
+                    return alert("Number " + james.value);
+                }
+            }
+        }
+    }
+}
+function isNumberError() {
+    for (var i = 0; i < james.value.length; i++) {
+        for (var j = 0; j < arrayNumber.length; j++) {
+            for (var m = 0; m < arrayWord.length; m++) {
+                if (james.value[0] == arrayNumber[j] && james.value[i] == arrayWord[m]) {
+                    a = ' Error';
+                    jara();
+                    finalizar == false;
+                    num == false;
+                    iden == false;
+                    sym == false;
+                    return alert("Error " + james.value);
+                }
+            }
+        }
+    }
+}
+function isSymbol() {
+    for (var i = 0; i < james.value.length; i++) {
+        for (var j = 0; j < arraySymbol.length; j++) {
+            if (james.value[i] == arraySymbol[j]) {
+                a = ' Symbol';
+                jara();
+                finalizar == false;
+                num == false;
+                iden == false;
+                numer == false;
+                alert("It's a Symbol " + james.value);
+            }
+        }
+    }
+
 }
